16ad69a86392f468eb0c237a58d1e4e779b874b8
wangjiefeng <wangjiefeng@baidu.com>
2020-02-11 21:48:56 +0800

    liloi

--- modified_code/classifier_data_lib.py
+++ modified_code/classifier_data_lib.py
@@ -402,7 +402,8 @@ class BdbkProcessor(DataProcessor):
 
     def get_labels(self):
         """See base class."""
-        return ["0", "1", "2", "3", "4", "5", "6", "7"]
+        #return ["0", "1", "2", "3", "4", "5", "6", "7"]
+        return ["poi", "出版物", "机构", "科技", "历史军事", "其他", "人物", "社会", "生活", "网络小说", "文化", "医疗", "娱乐", "自然", "组织"]
 
     @staticmethod
     def get_processor_name():
