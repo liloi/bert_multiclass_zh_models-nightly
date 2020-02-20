3aa0c09d0c9806405dcacef43e72a87bcd9c65e1
wangjiefeng <wangjiefeng@baidu.com>
2020-02-20 16:37:33 +0800

    liloi

--- modified_code/classifier_data_lib.py
+++ modified_code/classifier_data_lib.py
@@ -97,6 +97,7 @@ class DataProcessor(object):
     """Reads a tab separated value file."""
     with tf.io.gfile.GFile(input_file, "r") as f:
       reader = csv.reader(f, delimiter="\t", quotechar=quotechar)
+      logging.info('%s length={}'.format(len(list(reader))))
       lines = []
       for line in reader:
         lines.append(line)
