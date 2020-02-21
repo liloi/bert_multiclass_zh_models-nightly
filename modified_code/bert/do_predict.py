031e897b30e9bfa08f11e7070140a80c99c97a37
wangjiefeng <wangjiefeng@baidu.com>
2020-02-21 22:00:58 +0800

    liloi

--- modified_code/bert/do_predict.py
+++ modified_code/bert/do_predict.py
@@ -66,6 +66,24 @@ def get_dataset_fn(input_file_pattern, max_seq_length, global_batch_size,
   return _dataset_fn
 
 
+def get_loss_fn(num_classes, loss_factor=1.0):
+  """Gets the classification loss function."""
+
+  def classification_loss_fn(labels, logits):
+    """Classification loss."""
+    labels = tf.squeeze(labels)
+    log_probs = tf.nn.log_softmax(logits, axis=-1)
+    one_hot_labels = tf.one_hot(
+        tf.cast(labels, dtype=tf.int32), depth=num_classes, dtype=tf.float32)
+    per_example_loss = -tf.reduce_sum(
+        tf.cast(one_hot_labels, dtype=tf.float32) * log_probs, axis=-1)
+    loss = tf.reduce_mean(per_example_loss)
+    loss *= loss_factor
+    return loss
+
+  return classification_loss_fn
+
+
 def main(_):
   # Users should always run this script under TF 2.x
   assert tf.version.VERSION.startswith('2.')
@@ -102,6 +120,7 @@ def main(_):
   predict_dataset = predict_input_fn()
   bert_model, sub_model = _get_classifier_model()
   optimizer = bert_model.optimizer
+  loss_fn = get_loss_fn(num_classes)
   new_model = tf.keras.models.load_model(
       FLAGS.model_export_path,
       custom_objects={"KerasLayer": sub_model,
